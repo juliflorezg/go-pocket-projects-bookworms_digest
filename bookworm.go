d49c6c1b323d55d87fa0b7b7bc2c3756ae987f8e
Julian Florez <forezjuli08@hotmail.com>
2024-07-31 16:21:00 -0500

    add - types for Book and Bookworm, JSON file decoding in a Go variable ( []Bookworm )

--- bookworm.go
+++ bookworm.go
@@ -1,19 +1,40 @@
 package main
 
-import "os"
+import (
+	"encoding/json"
+	"os"
+)
+
+// A Bookworm contains the list of books on bookworm's shelf
+type Bookworm struct {
+	Name  string `json:"name"`
+	Books []Book `json:"books"`
+}
+
+// Book describes book information on a bookworm's shelf
+type Book struct {
+	Author string `json:"author"`
+	Title  string `json:"title"`
+}
 
 // loadBookworms reads the file and returns the list of
 // bookworms, and their beloved books, found therein.
 func loadBookworms(filePath string) ([]Bookworm, error) {
-
 	f, err := os.Open(filePath)
 	if err != nil {
 		return nil, err
 	}
 	defer f.Close()
 
-	return f, nil
+	// Initialize the type in which the file will be decoded.
+	var bookworms []Bookworm
 
-	// return nil, nil
+	// Decode the file and store the content in the variable bookworms.
+	err = json.NewDecoder(f).Decode(&bookworms)
 
+	if err != nil {
+		return nil, err
+	}
+
+	return bookworms, nil
 }
