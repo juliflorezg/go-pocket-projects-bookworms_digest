471e95f6ed9e8c9428cfa99d859c9b4d4a650b77
Julian Florez <forezjuli08@hotmail.com>
2024-08-01 20:02:05 -0500

    add - comparing slices

--- main.go
+++ main.go
@@ -13,4 +13,17 @@ func main() {
 	}
 	fmt.Printf("%+v\n", bookworms)
 	fmt.Printf("%v\n", bookworms)
+
+	fmt.Printf("book count: %+v\n", booksCount(bookworms))
+	emptyBookworms := []Bookworm{}
+	var emptyBookworms2 []Bookworm
+	fmt.Printf("no bookworms: %+v\n", booksCount(emptyBookworms))
+
+	// ! go won't even let the comparison happen:
+	// ! invalid operation: emptyBookworms == emptyBookworms (slice can only be compared to nil)
+	// fmt.Printf("comparing empty bookworms slice to itself: %v\n", emptyBookworms == emptyBookworms)
+	fmt.Printf("comparing emptyBookworms slice to nil: %v\n", emptyBookworms == nil)
+	fmt.Printf("comparing emptyBookworms2 slice to nil: %v\n", emptyBookworms2 == nil)
+	fmt.Println(len(emptyBookworms))
+	fmt.Println(len(emptyBookworms2))
 }
